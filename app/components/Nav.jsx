4c1dcba0b0b15a04a05cd448d7bbd790e72ad6bc
João K <jk.designer10@gmail.com>
2017-02-09 17:46:13 -0200

    Style Nav Component

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,33 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () => {
-  return(
-    <nav className="main-nav">
-      <h1 className="logotype">ReactWeather</h1>
-      <IndexLink activeClassName="-active" to="/">Get Weather</IndexLink>
-      <Link activeClassName="-active" to="/about">About</Link>
-      <Link activeClassName="-active" to="/examples">Examples</Link>
-    </nav>
-  );
-}
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert('Not yet wired up...');
+  },
+  render: function() {
+    return(
+      <header className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li><span className="menu-text">React Weather App</span></li>
+            <li><IndexLink activeClassName="-active" to="/">Get Weather</IndexLink></li>
+            <li><Link activeClassName="-active" to="/about">About</Link></li>
+            <li><Link activeClassName="-active" to="/examples">Examples</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li><input type="text" placeholder="Search weather"/></li>
+              <li><input type="submit" className="button" value="Get Weather"/></li>
+            </ul>
+          </form>
+        </div>
+      </header>
+    );
+  }
+});
 
 module.exports = Nav;
